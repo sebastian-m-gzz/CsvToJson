e53f774c9e55f44c67bcd5197b2e4899a69f0021
sebastian-m-gzz <sebastian.maronas@enroutesystems.com>
2020-11-27 15:57:33 -0600

    Comments and removed test http argument

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, request
-from csv import reader, DictReader
+from flask import Flask, jsonify
+from csv import DictReader
 
 app = Flask(__name__)
 
 
 def read(file_path):
+    """
+    Reads the given file path as a csv file and parses its data
+    into a list of dictionaries.
+    """
     items = []
     with open(file_path, 'r') as csv_file:
         file_reader = DictReader(csv_file)
@@ -13,11 +17,13 @@ def read(file_path):
     return items
 
 
-# http://host:5000/
 @app.route('/', methods=['GET'])
 def main():
-    file_name = request.args.get('name', 'employees', type=str)
-    data = read(f'data/{file_name}.csv')
+    """
+    Entry point of application
+    """
+    file_name = 'employees.csv'
+    data = read(f'data/{file_name}')
     return jsonify(data)
 
 
