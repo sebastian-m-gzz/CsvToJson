4c65df55fc0167e4d6f283af71deef1231e22dca
sebastian-m-gzz <sebastian.maronas@enroutesystems.com>
2020-11-27 15:13:22 -0600

    First commit

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from csv import reader, DictReader
+
+app = Flask(__name__)
+
+
+def read(file_path):
+    items = []
+    with open(file_path, 'r') as csv_file:
+        file_reader = DictReader(csv_file)
+        for dictionary in file_reader:
+            items.append(dictionary)
+    return items
+
+
+# http://host:5000/
+@app.route('/', methods=['GET'])
+def main():
+    file_name = request.args.get('name', 'data', type = str)
+    data = read(f'data/{file_name}.csv')
+    return jsonify(data)
+
+
+app.run(debug=True)
